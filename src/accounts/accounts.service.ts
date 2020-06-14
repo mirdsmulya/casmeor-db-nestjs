cf0dfc1a09a0cb79ead764320378dac5fb2d303f
mirdan <syahidmirdan@gmal.com>
2020-06-14 15:29:49 +0700

    Add InjectRepository for account

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Account } from './accounts.entity';
+import { Repository } from 'typeorm';
 
 
 @Injectable()
-
 export class AccountsService {
+    constructor(
+        @InjectRepository(Account)
+        private accountRepository: Repository<Account>,
+    ) {}
+    
     findAll() {
         return "HIII"
     }
