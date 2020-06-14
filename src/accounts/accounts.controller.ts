eac8680a62b5e5fa08c5745952623039089497ba
mirdan <syahidmirdan@gmal.com>
2020-06-14 18:21:15 +0700

    Add CRUD mechanism on controller and service

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common'
+import { Controller, Get, Post, Delete, Body, Param, Put } from '@nestjs/common'
 import { AccountsService } from './accounts.service'
-import { Account } from './interfaces/account.interface';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { UpdateAccountDto } from './dto/update-account.dto'
 
 
 @Controller('accounts')
@@ -9,9 +10,28 @@ export class AccountsController {
     constructor(private accountService: AccountsService) {}
 
     @Get()
-    // findAll(): Promise<Account[]> {
     findAll() {
         return this.accountService.findAll()
+    }
+
+    @Get(':id')
+    findOne(@Param() account) {
+        return this.accountService.findOne(account.id)
+    }
+    
+    @Delete(':id')
+    remove(@Param() account) {
+        return this.accountService.remove(account.id)
+    }
+
+    @Post()
+    create(@Body() createAccountDto: CreateAccountDto) {
+        return this.accountService.saveAccount(createAccountDto)
+    }
+
+    @Put(':id')
+    update(@Param() id: string, @Body() updateAccountDto: UpdateAccountDto)  {
+        return this.accountService.updateAccount(id, updateAccountDto)
     }
 
 
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -11,8 +11,25 @@ export class AccountsService {
         private accountRepository: Repository<Account>,
     ) {}
     
-    findAll() {
-        return "HIII"
+    findAll(): Promise<Account[]> {
+        return this.accountRepository.find();
+    }
+
+    findOne(id: string): Promise<Account> {
+        return this.accountRepository.findOne(id);
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.accountRepository.delete(id)
+    }
+
+    async saveAccount(account: object): Promise<void> {
+        await this.accountRepository.save(account)
+    }
+
+    async updateAccount(id: string, account: object): Promise<void> {
+        await this.accountRepository.update(id, account)
+
     }
     
 }
