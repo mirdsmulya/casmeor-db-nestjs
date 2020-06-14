9275f5803ca61fe5c4fe01aa6b372ac717d420b5
mirdan <syahidmirdan@gmal.com>
2020-06-14 18:21:31 +0700

    Add Data Transfer Object

---
+++ src/accounts/dto/create-account.dto.ts
@@ -0,0 +1,8 @@
+export class CreateAccountDto {
+    name: string;
+    username: string;
+    role: string;
+    password: string;
+    nip: string;
+    id:string;
+}
---
+++ src/accounts/dto/update-account.dto.ts
@@ -0,0 +1,8 @@
+export class UpdateAccountDto {
+    name: string;
+    username: string;
+    role: string;
+    password: string;
+    nip: string;
+    id:string;
+}
