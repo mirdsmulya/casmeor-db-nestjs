406146226e1659a8f249eecdff1d414a64962c73
mirdan <syahidmirdan@gmal.com>
2020-06-28 19:02:16 +0700

    Add OrderListService to OrderService

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './orders.entity';
 import { Repository } from 'typeorm';
+import { OrderList } from 'src/orderList/orderList.entity';
+import { OrderListService } from 'src/orderList/orderList.service';
 
 
 @Injectable()
@@ -9,6 +11,7 @@ export class OrdersService {
     constructor(
         @InjectRepository(Order)
         private orderRepository: Repository<Order>,
+        private orderListService: OrderListService
     ) {}
     
     findAll(): Promise<Order[]> {
@@ -23,13 +26,15 @@ export class OrdersService {
         await this.orderRepository.delete(id)
     }
 
-    async saveOrder(order: object): Promise<void> {
+    async saveOrder(order: object, orderList: Array<OrderList>): Promise<void> {
         await this.orderRepository.save(order)
+        await this.orderListService.saveOrderList(orderList)
     }
 
-    async updateOrder(id: string, order: object): Promise<void> {
-        await this.orderRepository.update(id, order)
-
+    async updateOrder(id: string, order: object, orderList: Array<OrderList>): Promise<void> {
+        await this.orderRepository.delete(id)
+        await this.orderRepository.save(order)
+        await this.orderListService.saveOrderList(orderList)
     }
     
 }
